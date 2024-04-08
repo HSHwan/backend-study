2a1de2e27653e89bfef6a56cebccd3884d6aaf9d
HSHwan <tnghks2866@gmail.com>
2024-04-08 16:48:03 +0900

    Add: DTO 객체 정의 (CH01-02)

---
+++ study-code/src/ch01/MemberDTO.java
@@ -0,0 +1,67 @@
+package ch01;
+
+public class MemberDTO {
+    private String name;
+    private int age;
+    private String phone;
+    private String email;
+    private String addr;
+    private float weight;
+
+    public MemberDTO(String name, int age, String phone, String email, String addr, float weight) {
+        this.name = name;
+        this.age = age;
+        this.phone = phone;
+        this.email = email;
+        this.addr = addr;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+
+    public float getWeight() {
+        return weight;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+}

a7d3dc6393def15433ff5e0e0ba16edb5ca0bd6a
HSHwan <tnghks2866@gmail.com>
2024-04-08 17:15:08 +0900

    Add: DTO 객체 인스턴스 생성 (CH01-03)

--- study-code/src/ch01/Person.java
+++ study-code/src/ch01/Person.java
@@ -5,10 +5,7 @@ public class Person {
     private int age;
     private String phone;
 
-    public Person(String name, int age, String phone) {
-        this.name = name;
-        this.age = age;
-        this.phone = phone;
+    public Person() {
     }
 
     public String getName() {
---
+++ study-code/src/ch01/PersonTest.java
@@ -0,0 +1,11 @@
+package ch01;
+
+public class PersonTest {
+
+    public static void main(String[] args) {
+        // Q. 한 사람의 데이터를 저장할 변수를 선언하세요.
+        // 모델링 된 Person 클래스를 이용하여 객체를 메모리에 생성하세요.
+        // 인스턴스 생성 과정
+        Person person = new Person();
+    }
+}

44e4dfee1574f5a3b61d39034a50cdf1a509162d
HSHwan <tnghks2866@gmail.com>
2024-04-12 19:03:33 +0900

    Feat: CarDTO 활용 (CH02-04)

---
+++ study-code/src/part3/ch02/CarUtilityTest.java
@@ -0,0 +1,29 @@
+package part3.ch02;
+
+import part3.model.CarDTO;
+import part3.model.CarUtility;
+
+public class CarUtilityTest {
+    public static void main(String[] args) {
+        // Q. Car 정보를 출력하는 동작을 가지고 있는 Utility 클래스를 설계하세요.
+        int carSerialNumber = 1110;
+        String carName = "BMW528i";
+        int carPrice = 90000000;
+        String carOwner = "홍길동";
+        int carYear = 2015;
+        String carType = "G";
+
+        // 데이터를 하나로 묶는다(이동) -> DTO
+        CarDTO car = new CarDTO(
+                carSerialNumber,
+                carName,
+                carPrice,
+                carOwner,
+                carYear,
+                carType);
+
+//        carPrint(car);
+        CarUtility carUtil = new CarUtility();
+        carUtil.carPrint(car);
+    }
+}
---
+++ study-code/src/part3/model/CarUtility.java
@@ -0,0 +1,7 @@
+package part3.model;
+
+public class CarUtility {
+    public void carPrint(CarDTO car) {
+        System.out.println(car);
+    }
+}

3f6f357788315217d9d61a73818a955d28fc3237
HSHwan <tnghks2866@gmail.com>
2024-04-19 13:58:19 +0900

    Add: static, non-static 멤버 접근 예시 [CH06-02]

---
+++ study-code/src/part3/ch06/NonStaticTest.java
@@ -0,0 +1,17 @@
+package part3.ch06;
+
+public class NonStaticTest {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        NonStaticTest nst = new NonStaticTest();
+        int sum = nst.hap(a, b);
+        System.out.println("sum = " + sum); // 30
+    }
+
+    public int hap(int a, int b) {
+        int v = a + b;
+        return v;
+    }
+}

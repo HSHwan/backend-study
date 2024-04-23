d90b64193d472f5ea1b07a2e9c0dafccd1b0d629
HSHwan <tnghks2866@gmail.com>
2024-04-23 19:57:12 +0900

    Add: 상위 클래스 정보은닉 확보 예시 [CH01-06,07]

--- study-code/src/part4/model/Employee.java
+++ study-code/src/part4/model/Employee.java
@@ -1,12 +1,12 @@
 package part4.model;
 // 사원(VO, DTO)
 public class Employee {
-    protected String name;
-    protected int age;
-    protected String phone;
-    protected String empDate;
-    protected String dept;
-    protected boolean marriage;
+    private String name;
+    private int age;
+    private String phone;
+    private String empDate;
+    private String dept;
+    private boolean marriage;
 
     public Employee(String name, int age, String phone, String empDate, String dept, boolean marriage) {
         super(); // 상위 클래스의 생성자 호출 -> new Object()
--- study-code/src/part4/model/RempVO.java
+++ study-code/src/part4/model/RempVO.java
@@ -2,8 +2,6 @@ package part4.model;
 // 일반사원(VO)
 public class RempVO extends Employee {
     public RempVO(String name, int age, String phone, String empDate, String dept, boolean marriage) {
-        super(name, age, phone, empDate, dept, marriage);
+        super(name, age, phone, empDate, dept, marriage); // 상위 클래스 초기화
     }
-
-
 }

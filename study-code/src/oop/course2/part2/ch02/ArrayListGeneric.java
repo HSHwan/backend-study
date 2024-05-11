7ec860477e8cbd30440f9b330f5fe53559f17186
HSHwan <tnghks2866@gmail.com>
2024-05-11 22:45:42 +0900

    add: ArrayList Generic 사용 예시 [CH02-02]

---
+++ study-code/src/oop/course2/part2/ch02/ArrayListGeneric.java
@@ -0,0 +1,18 @@
+package oop.course2.part2.ch02;
+
+import oop.course2.part2.model.Movie;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListGeneric {
+
+    public static void main(String[] args) {
+        List<Movie> list = new ArrayList<>(); // Movie[]
+        list.add(new Movie("괴물", "봉준호", "2006", "한국"));
+//        list.add("Hello"); // X
+        list.add(new Movie("기생충", "봉준호", "2019", "한국"));
+        System.out.println(list.get(0));
+        System.out.println(list.get(1));
+    }
+}

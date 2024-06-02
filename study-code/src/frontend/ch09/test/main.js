c78e65f3d8aa6ec1875fcd5b3a9e112e6278dc46
HSHwan <tnghks2866@gmail.com>
2024-06-02 19:20:35 +0900

    add: JS 자료형 예시 추가 [CH09-02]

--- study-code/src/frontend/ch09/test/main.js
+++ study-code/src/frontend/ch09/test/main.js
@@ -1,1 +1,14 @@
-console.log('HEROPY');
+let myName = "HEROPY";
+let email = 'thesecon@gmail.com';
+let hello = `Hello ${myName}?!`
+let user = {
+    name: 'HEROPY',
+    age: 85
+};
+
+console.log(myName);
+console.log(email);
+console.log(hello);
+console.log(user)
+console.log(user.age)
+console.log(user.name)
